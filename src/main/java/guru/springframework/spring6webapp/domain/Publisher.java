9bf3741c228c55aadcaffb815676cf42c36ff7b3
QuaterByte <kareemazeez256@gmail.com>
2024-12-09 14:43:13 +0100

    Added Publisher domain and Repository

--- src/main/java/guru/springframework/spring6webapp/domain/Publisher.java
+++ src/main/java/guru/springframework/spring6webapp/domain/Publisher.java
@@ -1,4 +1,84 @@
 package guru.springframework.spring6webapp.domain;
 
+import jakarta.persistence.*;
+
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
 public class Publisher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String publisherName;
+    private String address;
+    private String zipcode;
+
+    @OneToMany(mappedBy = "publisher")
+    private Set<Book> books;
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPublisherName() {
+        return publisherName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setPublisherName(String publisherName) {
+        this.publisherName = publisherName;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    @Override
+    public String toString() {
+        return "Publisher{" +
+                "id=" + id +
+                ", publisherName='" + publisherName + '\'' +
+                ", address='" + address + '\'' +
+                ", zipcode='" + zipcode + '\'' +
+                ", books=" + books +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Publisher publisher = (Publisher) o;
+        return Objects.equals(id, publisher.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
